04f2666c5f0889b73b258e61a1717e0c6c018f91
Alchemist-X <55140230+Alchemist-X@users.noreply.github.com>
2020-11-18 12:12:56 +0800

    add monster content

--- data_chracters.h
+++ data_chracters.h
@@ -46,9 +46,40 @@ Thanos.bossAttackPower=100;
 Thanos.bossDefensivePower=80;
 Thanos.bossAgility=70;
 
+// Monsters' design is similar to Boss's
+struct Monster
+{
+	string monsterName;
+	double gold;
+	double monsterHealthPoint;
+	double monsterAttackPower;
+	double monsterDefensivePower;
+	double monsterAgility;
+}
 
+Monster Orc;
+Orc.monsterName="Orc";
+Orc.gold=20;
+Orc.monsterHealthPoint=30;
+Orc.monsterAttackPower=20;
+Orc.monsterDefensivePower=5;
+Orc.monsterAgility=10;
 
+Monster Slime;
+Slime.monsterName="Slime";
+Slime.gold=5;
+Slime.monsterHealthPoint=90;
+Slime.monsterAttackPower=5;
+Slime.monsterDefensivePower=15;
+Slime.monsterAgility=5;
 
+Monster FallenElf;
+FallenElf.monsterName="FallenElf";
+FallenElf.gold=200;
+FallenElf.monsterHealthPoint=45;
+FallenElf.monsterAttackPower=20;
+FallenElf.monsterDefensivePower=1;
+FallenElf.monsterAgility=70;
 
 // we designed a few skills
 
