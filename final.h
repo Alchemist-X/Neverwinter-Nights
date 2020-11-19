de73869fb5328dc164f61dfa43153087e7b2aaf7
Alchemist-X <55140230+Alchemist-X@users.noreply.github.com>
2020-11-19 23:43:13 +0800

    Add files via upload

---
+++ final.h
@@ -0,0 +1,17 @@
+# include <string>
+# include <iostream>
+using namespace std;
+
+bool final()
+{
+    bool end;
+    cout << "Your blade inserts the chest of Thanos \n"
+    "It's ...... impossible!!! Thanos yell out \n"
+    "Within second, the giant creature falls dawn and turn into ahses immediately \n"
+    "In the ground, you suprisingly find the ... !!! \n"
+    "The condemned heart of fallen Lich !!!! \n"
+    "Congratulation! warrior, with this legendary item, you are now able to end the tragedy in the world of Neverwinter Nights" << endl;
+
+    end = true;
+    return end;
+}
