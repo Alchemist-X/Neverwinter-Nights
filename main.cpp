1d7e3d62e7cbe1198d3c4148bcad63db118d6262
wsjack22 <59447424+wsjack22@users.noreply.github.com>
2020-11-19 17:15:33 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <cmath>
 #include <fstream>
-#include "game_data.h"
+#include "data_items.h"
+#include "data_chracters.h"
 #include "status.h"
 #include <string>
 #include <sstream>
@@ -12,9 +13,44 @@ using namespace std;
 
 //in this cpp file, we will build the fight scene. 
 
+// there are several pivotal functions in this program.
+
+// we want to check if the hero is alive at the end of each round 
+bool isHeroAlive(int healthPoint)
+{
+	if(healthPoint>0){
+		return 1;
+	}
+	else
+		return 0;
+}
+
+// we want to check if the boss is alive at the end of each round
+bool isEnemyAlive(int enemyHealthPoint)
+{
+	if(enemyHealthPoint>0){
+		return 1;
+	}
+	else
+		return 0;
+}
 
 
-int main(){
+// we want to confirm if the player wants to escape from the fight
+void isEscape(int escape)
+{
+	if (escape==4){
+		flag = 0;
+	}
+	else
+		flag = 1;
+}
+
+
+
+int main()
+
+{
 	// first of all, we want to welcome the players and explain this game. 
 	cout << "Welcome to this game of adventure!" <<endl;
 
@@ -53,7 +89,7 @@ int main(){
 
 
 	ifstream fin;
-	fin.open("character_status.txt");
+	fin.open("save.txt");
 	// in all *status.txt, all the data have the following format：
 	// character name: 
 	// game level that the player in:
@@ -112,12 +148,132 @@ int main(){
 	// we read the data from the file of character's status
 	fin.close();
 
-
-
+	// we will build the fight scene here
 	
+	bool flag =1;
+	int round=1;
+	
+		//cout << "Please choose a Monster or a Boss to fight." << endl;
+		//cout << "Please tpye a number to choose." << endl;
+		//cout << "Enter 1 to battle with Monster Orc." << endl;
+		//cout << "Enter 2 to battle with Monster Slime." << endl;
+		//cout << "Enter 3 to battle with Monster FallenElf." << endl;
+		//cout << "Enter 4 to battle with Boss Riddler." << endl;
+		//cout << "Enter 5 to battle with Boss Joker." << endl;
+		//cout << "Enter 6 to battle with Boss Thanos." << endl;
 
+		//int choiceOfEnemy;
+		Enemy enemyToBattle;
+		//cin >> choiceOfEnemy;
+
+		cout << "You will chanllenge " << enemyToBattle.enemyName << " this time." endl;
+		cout << "Your agility is " << agility << "  " << "Your enemy's agility is " << enemyToBattle.enemyAgility << endl;
+
+		// compare the agility of the hero and enemy, to determine whether the hero or the enemy fight first.
+		if (agility <= enemyToBattle.enemyAgility)
+		{
+			cout << "Enemy will fight first." << endl;
+			int heroTurn=1;
+			int enemyTurn=2;
+			int turn = enemyTurn;
+
+
+			while(flag)
+			{
+				cout << "This is the Round " << round << " of the fight." << endl;
+
+				// we will check if it's hero's turn or enemy's turn.
+				
+				if (turn == enemyTurn)
+				// now it's enenmy's turn
+				{
+				cout << "This is the enemy's turn of fight." << endl;
+
+				healthPoint = healthPoint + defensivePower - enemyToBattle.enemyAttackPower;
+
+				cout << "You have suffered " << - defensivePower + enemyToBattle.enemyAttackPower << "point of ATK from the enemy." << endl;
+				cout << "Your current HP is " << healthPoint << endl;
+
+				}
+
+
+
+				else
+				// now it's hero's turn
+				{
+				cout << "Now it's your turn." << endl;
+				cout << "Please input a number to indicate your choice." << endl;
+				cout << "Enter 1 to give the enemy a common attack." << endl;
+				cout << "Enter 2 to use your potion." << endl;
+				cout << "Enter 3 to give the enemy a skill attack." << endl;
+				cout << "Enter 4 to escape from the fight." << endl;
+
+				int choiceOfTheRound;
+				cin >> choiceOfTheRound;
+				switch (choiceOfTheRound)
+				{
+					case 1:
+					// the player choose to give a common attack
+					
+					enemyToBattle.enemyHealthPoint = enemyToBattle.enemyHealthPoint + enemyToBattle.enemyDefensivePower - attackPower;
+					cout << "The enemy have suffered " << - enemyToBattle.enemyDefensivePower + attackPower << "point of ATK from you." << endl;
+					cout << "Enemy's current HP is " << enemyToBattle.enemyHealthPoint << endl;
+					break;
+
+					case 2:
+					// the player choose to use the potion
+					
+					cout << "Please enter the name of the potion name that you want to use." << endl;
+					cout << "There are these potions in your package ('Empty' means no potion in the package): " <<endl;
+					cout << "Potion_1 : " << potion_1 << endl;
+					cout << "Potion_2 : " << potion_2 << endl;
+					cout << "Potion_3 : " << potion_3 << endl;
+					cout << "Please enter 'Empty' if your package is empty as well. " << enld;
+
+					string choiceOfThePotion;
+					cin >> choiceOfThePotion;
+					potionPower(choiceOfThePotion);
+					break;
+
+					case 3:
+					// the player chooses to use the skills
+					skillPower();
+					break;
+
+					case 4:
+					// the player chooses to escape from the fight
+					isEscape(choiceOfTheRound);
+					break;
+
+					default:
+					cout << "Invalid input! " << endl;
+
+
+				}
+
+
+				}
+
+
+				// we will check if the battle came to an end.
+
+				turn = heroTurn;
+
+				round++;
+			}
+
+		}
+
+
+		else
+		{
+			cout << "You will fight first." << endl;
+			while(flag)
+			{
+
+			}
+		}
 }
-
 
 
 
