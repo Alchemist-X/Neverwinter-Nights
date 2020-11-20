fa4f4ad42218b2c2bf3cc01a727c201b7f9c8fa0
wsjack22 <59447424+wsjack22@users.noreply.github.com>
2020-11-20 16:35:25 +0800

    Add files via upload

---
+++ data_characters.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include "data_characters.h"
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+// In this file, we describe elements that are related with charcterr, namely player and enemies
+
+
+// enemy one, the Riddler
+
+
+Enemy Riddler;
+Riddler.enemyName="Riddler";
+Riddler.gold=100;
+Riddler.enemyHealthPoint=100;
+Riddler.enemyAttackPower=30;
+Riddler.enemyDefensivePower=20;
+Riddler.enemyAgility=10;
+
+
+// enemy two, the Joker
+
+
+Enemy Joker;
+Joker.enemyName="Joker";
+Joker.gold=200;
+Joker.enemyHealthPoint=150;
+Joker.enemyAttackPower=60;
+Joker.enemyDefensivePower=30;
+Joker.enemyAgility=40;
+
+
+// enemy three, the Thanos
+
+
+Enemy Thanos;
+Thanos.enemyName="Thanos";
+Thanos.gold=500;
+Thanos.enemyHealthPoint=300;
+Thanos.enemyAttackPower=100;
+Thanos.enemyDefensivePower=80;
+Thanos.enemyAgility=70;
+
+
+// there are three little enemies we can percieve them as Monsters.
+
+
+Enemy Orc;
+Orc.enemyName="Orc";
+Orc.gold=20;
+Orc.enemyHealthPoint=30;
+Orc.enemyAttackPower=20;
+Orc.enemyDefensivePower=5;
+Orc.enemyAgility=10;
+
+Enemy Slime;
+Slime.enemyName="Slime";
+Slime.gold=5;
+Slime.enemyHealthPoint=90;
+Slime.enemyAttackPower=5;
+Slime.enemyDefensivePower=15;
+Slime.enemyAgility=5;
+
+Enemy FallenElf;
+FallenElf.enemyName="FallenElf";
+FallenElf.gold=200;
+FallenElf.enemyHealthPoint=45;
+FallenElf.enemyAttackPower=20;
+FallenElf.enemyDefensivePower=1;
+FallenElf.enemyAgility=70;
+
+// we designed a few skills
+
+
+Skill crit;
+crit.skillName="Crit";
+
+Skill undefense;
+undefense.skillName="Undefense";
+
+Skill miss;
+miss.skillName="Miss";
+
+Skill reverse;
+reverse.skillName="Reverse";
+
+
+void skillPower()
+{
+	srand(time(NULL));
+	int skillRandomNumber;
+	skillRandomNumber=rand()%4+1;
+	switch (skillRandomNumber)
+	{
+		case 1:
+		cout << "You are using the Crit skill now." << endl;
+		enemyHealthPoint-=100;
+		break;
+
+		case 2:
+		cout << "You are using the Undefense skill now." << endl;
+		enemyDefensivePower=0;
+		break;
+
+		case 3:
+		cout << "You are using the Miss skill now." << endl;
+		healthpoint+=(enemyAttackPower - defensivePower);
+		break;
+
+		case 4:
+		cout << "You are using the Reverse skill now." << endl;
+		int tempHealth=healthpoint;
+		int tempAttack=attackPower;
+		int tempDefense=defensivePower;
+
+		healthpoint=enemyHealthPoint;
+		attackPower=enemyAttackPower;
+		defensivePower=enemyDefensivePower;
+
+		enemyHealthPoint=tempHealth;
+		enemyAttackPower=tempAttack;
+		enemyDefensivePower=tempDefense;
+		break;
+
+	}
+
+}
+
+
+
