869cb4af89ab5eec0a8c05e9bdd39466502c547d
wsjack22 <59447424+wsjack22@users.noreply.github.com>
2020-11-20 16:38:38 +0800

    Add files via upload

---
+++ scenes.h
@@ -0,0 +1,17 @@
+#ifndef SCENES_H
+#define SCENES_H
+
+// this function shows what player will come across in the Inn.
+void enterInn();
+
+// this function shows what player will come across in the Shop.
+void enterShop();
+
+
+// this function shows what player will come across in the Church.
+void enterChurch();
+
+// this function shows what player will come across in the Bar.
+void enterBar();
+
+#endif
