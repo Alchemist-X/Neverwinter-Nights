e6b55fdbe5df9a2b1eb6a865031d772708bdb949
wsjack22 <59447424+wsjack22@users.noreply.github.com>
2020-11-20 16:49:39 +0800

    Update final.h

--- final.h
+++ final.h
@@ -1,15 +1,6 @@
-// This is the final chapter of the game
+#ifndef FINAL_H
+#define FINAL_H
 
-bool final()
-{
+bool final();
 
-    cout << "Your blade inserts the chest of Thanos \n"
-    "It's ...... impossible!!! Thanos yell out \n"
-    "Within second, the giant creature falls dawn and turn into ahses immediately \n"
-    "In the ground, you suprisingly find the ... !!! \n"
-    "The condemned heart of fallen Lich !!!! \n"
-    "Congratulation! warrior, with this legendary item, you are now able to end the tragedy in the world of Neverwinter Nights" << endl;
-
-    continueProgram = false;
-    return continueProgram;
-}
+#endif
